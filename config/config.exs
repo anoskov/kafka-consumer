c2e0722c94ee171e576de9bb0f15446b6f0d631f
Andrey Noskov <anoskov@ccsteam.ru>
2016-08-11 20:55:44 +0300

    use kafka_ex config

--- config/config.exs
+++ config/config.exs
@@ -1,11 +1,9 @@
 use Mix.Config
 
 config :kafka_ex,
-  brokers: [
-    {Application.get_env(:kafka_consumer, :kafka_host) || System.get_env("KAFKA_HOST") || "localhost", 9092}
-  ],
-  consumer_group: Application.get_env(:kafka_consumer, :consumer_group) || "kafka_ex" ,
+  brokers: Application.get_env(:kafka_ex, :brokers) || [{"localhost", 9092}],
+  consumer_group: Application.get_env(:kafka_ex, :consumer_group) || "kafka_ex",
   disable_default_worker: false,
-  sync_timeout: Application.get_env(:kafka_consumer, :sync_timeout) || 3000,
-  max_restarts: Application.get_env(:kafka_consumer, :max_restarts) || 10,
-  max_seconds: Application.get_env(:kafka_consumer, :max_seconds) || 60
+  sync_timeout: Application.get_env(:kafka_ex, :sync_timeout) || 3000,
+  max_restarts: Application.get_env(:kafka_ex, :max_restarts) || 10,
+  max_seconds: Application.get_env(:kafka_ex, :max_seconds) || 60

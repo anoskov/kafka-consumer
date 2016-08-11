2d03fafb62c8fca2a037a70fdd25fff411b51067
Yuri Artemev <i@artemeff.com>
2016-08-12 00:44:05 +0300

    add config for test env

---
+++ config/test.exs
@@ -0,0 +1,9 @@
+use Mix.Config
+
+config :kafka_ex,
+  brokers: [{"localhost", 9092}],
+  consumer_group: "kafka_ex",
+  disable_default_worker: false,
+  sync_timeout: 3000,
+  max_restarts: 10,
+  max_seconds: 60

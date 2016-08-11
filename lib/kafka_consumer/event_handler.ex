383f38755f4bcd6ed58704219cf9203c80fe55b1
Andrey Noskov <anoskov@ccsteam.ru>
2016-08-11 21:09:46 +0300

    upgrade event_handler behaviour

--- lib/kafka_consumer/event_handler.ex
+++ lib/kafka_consumer/event_handler.ex
@@ -1,8 +1,32 @@
 defmodule KafkaConsumer.EventHandler do
   use Behaviour
 
+  @type payload :: {String.t, number, map}
+
+  @doc """
+  Start Event Handler
+  """
+  @callback start_link(term()) :: {atom, pid} | {atom, {atom, pid}}
+
   @doc """
   Handle event from topic
   """
-  @callback handle_event(pid, {String.t, number, Map.t}) :: {atom, Map.t}
+  @callback handle_event(pid, payload) :: {atom, map}
+
+  defmacro __using__(_) do
+    quote do
+      @behaviour unquote(__MODULE__)
+      use GenServer
+
+      def start_link(args) do
+        GenServer.start_link(__MODULE__, args, [])
+      end
+
+      def handle_event(pid, payload) do
+        GenServer.cast(pid, payload)
+      end
+
+      defoverridable start_link: 1, handle_event: 2
+    end
+  end
 end
